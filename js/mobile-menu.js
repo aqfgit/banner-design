3472797a37b05185c69b699a9952e3e13db0b8db
Bartek <aqfgit@gmail.com>
2018-05-01 23:11:41 +0200

    submenu offers almost done

--- js/mobile-menu.js
+++ js/mobile-menu.js
@@ -1,7 +1,56 @@
 ( function() {
   var button = document.querySelector( '.c-top-bar__nav-toggler' );
   var socials = document.querySelector('.c-top-bar__socials')
-	
+  var siteNav = document.querySelector('.c-top-bar__site-nav');
+  var offersSubmenuLabel = document.querySelector('.l-submenu__label');
+  var subMenu = document.querySelector('.l-submenu');
+
+  let lastSubmenuEvent;
+
+  function setProperEventToOpenSumbenu() {
+
+    let hoverAddClass = function() {
+      subMenu.classList.add('l-submenu--open');
+    }
+
+    let hoverRemoveClass = function() {
+      subMenu.classList.remove('l-submenu--open');
+    }
+
+    let clickToggleClass = function() {
+        subMenu.classList.toggle('l-submenu--open');
+    }
+
+    if (subMenu.classList.contains('l-submenu--open')) {
+      subMenu.classList.remove('l-submenu--open')
+    }
+
+    offersSubmenuLabel.removeEventListener('click', clickToggleClass, false);
+    offersSubmenuLabel.removeEventListener('mouseenter', hoverAddClass, false);
+    offersSubmenuLabel.removeEventListener('mouseleave', hoverRemoveClass, false);
+    
+    if (window.innerWidth > 1025) {
+      console.log('hover')
+      offersSubmenuLabel.removeEventListener('click', clickToggleClass, false);
+
+      offersSubmenuLabel.addEventListener('mouseenter', hoverAddClass, false);
+      offersSubmenuLabel.addEventListener('mouseleave',  hoverRemoveClass, false);
+
+    } else if (window.innerWidth <= 1025) {
+      console.log('click')
+      offersSubmenuLabel.removeEventListener('mouseenter', hoverAddClass, false);
+      offersSubmenuLabel.removeEventListener('mouseleave', hoverRemoveClass, false);
+
+      offersSubmenuLabel.addEventListener('click', clickToggleClass, false)
+    }
+    
+  }
+
+  setProperEventToOpenSumbenu();
+
+  window.addEventListener('resize', setProperEventToOpenSumbenu, false);
+ 
+
 	button.addEventListener( 'click', function( evt ) {
 		var hasBeenRotaded = button.style.transform === 'rotate(90deg)';
 		if (hasBeenRotaded) {
@@ -15,5 +64,6 @@
     
     socials.classList.toggle('c-top-bar__socials--open');
 		button.setAttribute( 'aria-expanded', String( nav.classList.contains( 'c-top-bar__site-nav--nav-open' ) ) );
-	}, false );
+  }, false );
+
 }());
